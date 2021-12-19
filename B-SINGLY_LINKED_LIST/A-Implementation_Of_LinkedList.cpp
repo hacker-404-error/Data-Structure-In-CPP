430812aed0cc3e25c072074fad8cb9affb37eca0
PRITAM DAS <er.pritamdas22@gmail.com>
2021-12-19 15:05:37 +0530

    Updated

---
+++ B-SINGLY_LINKED_LIST/A-Implementation_Of_LinkedList.cpp
@@ -0,0 +1,147 @@
+
+// USING: NODE AS A STRUCTURE
+
+/*
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node* next;
+
+};
+class linked_list
+{
+    private:
+     node*head=NULL;
+     node*tail=NULL;
+    
+    public:
+     void insert_elements();
+     void display();
+};
+
+void linked_list :: insert_elements()
+{
+    node*temp=new node;
+    cout<<"enter the data : ";
+    cin>>temp->data;
+    temp->next=NULL;
+    if(head==NULL)
+    {
+        head=temp;
+        tail=temp;
+    }
+    else
+    {
+        tail->next=temp;
+        tail=tail->next;    
+    }   
+ }
+
+void linked_list :: display()
+{
+    node*x;
+    x=head;
+    cout<<"your linked list is : "<<endl;
+    while(x!=tail)
+    {
+        cout<<x->data<<"->";
+        x=x->next;
+    }
+    cout<<x->data;
+}
+
+int main()
+{
+    system("cls");
+    linked_list a;
+    int n;
+    cout<<"enter the number of elements you want to enter"<<endl;
+    cin>>n;
+    while(n!=0)
+    {
+      a.insert_elements();
+      n--;
+    }
+    cout<<endl<<endl<<endl;
+    a.display();
+    getch();
+    return 0;
+}
+*/
+
+// USING:  NODE AS A CLASS 
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class node
+{
+    public:
+        int data;
+        node* next;
+
+};
+class linked_list
+{
+    private:
+     node*head=NULL;
+     node*tail=NULL;
+    
+    public:
+     void insert_elements();
+     void display();
+};
+
+void linked_list :: insert_elements()
+{
+    node*temp=new node;
+    cin>>temp->data;
+    temp->next=NULL;
+    if(head==NULL)
+    {
+        head=temp;
+        tail=temp;
+    }
+    else
+    {
+        tail->next=temp;
+        tail=tail->next;    
+    }   
+ }
+
+void linked_list :: display()
+{
+    node*x;
+    x=head;
+    cout<<"your linked list is : "<<endl;
+    while(x!=tail)
+    {
+        cout<<x->data<<"->";
+        x=x->next;
+    }
+    cout<<x->data;
+}
+
+int main()
+{
+    system("cls");
+    linked_list a;
+    int n;
+    cout<<"enter the number of elements you want to enter"<<endl;
+    cin>>n;
+    cout<<"enter the data : ";
+    while(n!=0)
+    {
+      a.insert_elements();
+      n--;
+    }
+    cout<<endl;
+    a.display();
+    getch();
+    return 0;
+}
+
---
+++ B-SINGLY_LINKED_LIST/C-Insert_At_Last.cpp
@@ -0,0 +1,94 @@
+// #include<iostream>
+// using namespace std;
+
+// struct node
+// {
+//     int data;
+//     node*next=NULL;
+
+// };
+
+// class linked
+// {
+//     private:
+//      node*head=NULL;
+//      node*tail=NULL;
+    
+//     public:
+//      void insert();
+//      void insert_at_last();
+//      void display();
+
+// };
+
+// void linked :: insert()
+// {
+//     node*temp=new node;
+//     cout<<"enter the element : ";
+//     cin>>temp->data;
+//     temp->next=NULL;
+//     if(head==NULL)
+//     {
+//         head=temp;
+//         tail=temp;
+//     }
+//     else
+//     {
+//         tail->next=temp;
+//         tail=tail->next;
+
+//     }
+// }
+
+// void linked :: display()
+// {
+//     node*x;
+//     x=head;
+//     cout<<"linked list : ";
+//     while(x!=tail)
+//     {
+//         cout<<x->data<<"-->";
+//         x=x->next;
+//     }
+//     cout<<x->data;
+// }
+
+// void linked :: insert_at_last()
+// {
+    
+//     node*temp=new node;
+//     cout<<"enter data : "<<endl;
+//     cin>>temp->data;
+//     tail->next=temp;
+//     tail=tail->next;
+// }
+
+// int main()
+// {
+//     linked a;
+//     int n;
+//     cout<<"enter the number of elements you want to enter : "<<endl;
+//     cin>>n;
+//     while(n!=0)
+//     {
+//         a.insert();
+//         n--;
+//     }
+//     a.display();
+//     int z;
+//     cout<<endl<<endl<<"do you want to insert at last : "<<endl;
+//     cin>>z;
+//     while(z==1)
+//     {
+//         a.insert_at_last();
+//          a.display();
+//          cout<<endl<<endl;
+//         cout<<"do you want to insert again : "<<endl;
+//         cin>>z;
+//     }
+//     cout<<endl;
+//     cout<<"final list is : ";
+//     a.display();
+//     return 0;
+// }
+
